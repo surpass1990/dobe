92957cdcbc36bb84e63f94d3e3e4ca972f53d701
zc.ding <zhichaoding@hongkun.com.cn>
2018-05-02 20:26:51 +0800

    初始化spring-boot项目

---
+++ src/main/java/com/dobe/dobe/DobeApplication.java
@@ -0,0 +1,12 @@
+package com.dobe.dobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobeApplication.class, args);
+	}
+}
